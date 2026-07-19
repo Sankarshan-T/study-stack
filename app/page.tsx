7b3e1a956b38eb05c82ac50d159f8314a7ceff18
CoolCream <mahasankarshant@gmail.com>
2026-07-19 10:29:23 +0530

    spent some time understanding the themes.css

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/base/buttons/button";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full">
-      <Button color="primary">Hello</Button>
+    <main className="h-screen w-full bg-bg-primary">
+
     </main>
   );
 }
