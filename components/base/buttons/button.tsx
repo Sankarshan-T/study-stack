961177880035ec02d9ae6788a20d5d87403c8841
CoolCream <mahasankarshant@gmail.com>
2026-07-19 10:19:43 +0530

    experimented with the ew untitled ui button, and fixed the import on the layout to point at the new globals.css that untitled ui has created

--- components/base/buttons/button.tsx
+++ components/base/buttons/button.tsx
@@ -158,7 +158,7 @@ export interface CommonProps {
 /**
  * Props for the button variant (non-link)
  */
-export interface ButtonProps extends CommonProps, Omit<AriaButtonProps, "children" | "className"> {}
+export interface ButtonProps extends CommonProps, Omit<AriaButtonProps, "children" | "className"> { }
 /**
  * Props for the link variant (anchor tag)
  */
@@ -185,75 +185,75 @@ export const Button: {
     showTextWhileLoading,
     ...props
 }) => {
-    const href = "href" in props ? props.href : undefined;
+        const href = "href" in props ? props.href : undefined;
 
-    const isIcon = (IconLeading || IconTrailing) && !children;
-    const isLinkType = ["link-gray", "link-color", "link-destructive"].includes(color);
+        const isIcon = (IconLeading || IconTrailing) && !children;
+        const isLinkType = ["link-gray", "link-color", "link-destructive"].includes(color);
 
-    noTextPadding = isLinkType || noTextPadding;
+        noTextPadding = isLinkType || noTextPadding;
 
-    const commonChildren = (
-        <>
-            {/* Leading icon */}
-            {isValidElement(IconLeading) && IconLeading}
-            {isReactComponent(IconLeading) && <IconLeading data-icon="leading" className={styles.common.icon} />}
+        const commonChildren = (
+            <>
+                {/* Leading icon */}
+                {isValidElement(IconLeading) && IconLeading}
+                {isReactComponent(IconLeading) && <IconLeading data-icon="leading" className={styles.common.icon} />}
 
-            {loading && (
-                <svg
-                    fill="none"
-                    data-icon="loading"
-                    viewBox="0 0 20 20"
-                    className={cx(styles.common.icon, !showTextWhileLoading && "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2")}
-                >
-                    {/* Background circle */}
-                    <circle className="stroke-current opacity-30" cx="10" cy="10" r="8" fill="none" strokeWidth="2" />
-                    {/* Spinning circle */}
-                    <circle
-                        className="origin-center animate-spin stroke-current"
-                        cx="10"
-                        cy="10"
-                        r="8"
+                {loading && (
+                    <svg
                         fill="none"
-                        strokeWidth="2"
-                        strokeDasharray="12.5 50"
-                        strokeLinecap="round"
-                    />
-                </svg>
-            )}
+                        data-icon="loading"
+                        viewBox="0 0 20 20"
+                        className={cx(styles.common.icon, !showTextWhileLoading && "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2")}
+                    >
+                        {/* Background circle */}
+                        <circle className="stroke-current opacity-30" cx="10" cy="10" r="8" fill="none" strokeWidth="2" />
+                        {/* Spinning circle */}
+                        <circle
+                            className="origin-center animate-spin stroke-current"
+                            cx="10"
+                            cy="10"
+                            r="8"
+                            fill="none"
+                            strokeWidth="2"
+                            strokeDasharray="12.5 50"
+                            strokeLinecap="round"
+                        />
+                    </svg>
+                )}
 
-            {children && (
-                <span data-text className={cx("transition-inherit-all", !noTextPadding && "px-0.5")}>
-                    {children}
-                </span>
-            )}
+                {children && (
+                    <span data-text className={cx("transition-inherit-all", !noTextPadding && "px-0.5")}>
+                        {children}
+                    </span>
+                )}
 
-            {/* Trailing icon */}
-            {isValidElement(IconTrailing) && IconTrailing}
-            {isReactComponent(IconTrailing) && <IconTrailing data-icon="trailing" className={styles.common.icon} />}
-        </>
-    );
+                {/* Trailing icon */}
+                {isValidElement(IconTrailing) && IconTrailing}
+                {isReactComponent(IconTrailing) && <IconTrailing data-icon="trailing" className={styles.common.icon} />}
+            </>
+        );
 
-    const commonProps = {
-        "data-loading": loading ? true : undefined,
-        "data-icon-only": isIcon ? true : undefined,
-        ...props,
-        isDisabled: disabled,
-        className: cx(
-            styles.common.root,
-            styles.sizes[size].root,
-            styles.colors[color].root,
-            isLinkType && styles.sizes[size].linkRoot,
-            (loading || (href && (disabled || loading))) && "pointer-events-none",
-            // If in `loading` state, hide everything except the loading icon (and text if `showTextWhileLoading` is true).
-            loading && (showTextWhileLoading ? "[&>*:not([data-icon=loading]):not([data-text])]:hidden" : "[&>*:not([data-icon=loading])]:invisible"),
-            className,
-        ),
-        children: commonChildren,
+        const commonProps = {
+            "data-loading": loading ? true : undefined,
+            "data-icon-only": isIcon ? true : undefined,
+            ...props,
+            isDisabled: disabled,
+            className: cx(
+                styles.common.root,
+                styles.sizes[size].root,
+                styles.colors[color].root,
+                isLinkType && styles.sizes[size].linkRoot,
+                (loading || (href && (disabled || loading))) && "pointer-events-none",
+                // If in `loading` state, hide everything except the loading icon (and text if `showTextWhileLoading` is true).
+                loading && (showTextWhileLoading ? "[&>*:not([data-icon=loading]):not([data-text])]:hidden" : "[&>*:not([data-icon=loading])]:invisible"),
+                className,
+            ),
+            children: commonChildren,
+        };
+
+        if ("href" in commonProps) {
+            return <AriaLink {...commonProps} href={disabled ? undefined : href} />;
+        }
+
+        return <AriaButton {...commonProps} type={commonProps.type || "button"} isPending={loading} />;
     };
-
-    if ("href" in commonProps) {
-        return <AriaLink {...commonProps} href={disabled ? undefined : href} />;
-    }
-
-    return <AriaButton {...commonProps} type={commonProps.type || "button"} isPending={loading} />;
-};
